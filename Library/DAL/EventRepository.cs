ae8c76c730b3ed1ecf521e944f7d2afafe6caa4a
REVISION-PC\Seth <sethetherald@duck.com>
2023-06-07 10:18:25 +0700

    Add initial Events repository and DTO.

---
+++ Library/DAL/EventRepository.cs
@@ -0,0 +1,24 @@
+﻿using Library.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library.DAL
+{
+    public class EventRepository : IEventRepository
+    {
+        private CoffeehouseSystemContext _context;
+
+        public EventRepository(CoffeehouseSystemContext context)
+        {
+            _context = context;
+        }
+
+        public List<Event> GetEvents()
+        {
+            return _context.Events.ToList();
+        }
+    }
+}
