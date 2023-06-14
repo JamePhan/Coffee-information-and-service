b8dbc1a24295011371a5819f2467d098b1d2ed87
REVISION-PC\Seth <sethetherald@duck.com>
2023-06-14 10:42:16 +0700

    Add get locations list, add and remove location.

---
+++ Library/DAL/ILocationRepository.cs
@@ -0,0 +1,20 @@
+﻿using Library.DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library.DAL
+{
+    public interface ILocationRepository : IDisposable
+    {
+        List<LocationInfo> GetLocations(int count);
+
+        void AddLocation(LocationInfo location);
+
+        void RemoveLocation(int locationId);
+
+        void Save();
+    }
+}
