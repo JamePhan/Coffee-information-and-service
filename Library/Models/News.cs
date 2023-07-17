6de9a8f1d2d729646bccfbb31cfc10fdae508550
REVISION-PC\Seth <sethetherald@duck.com>
2023-07-17 09:55:20 +0700

    Regenerate EF models.

---
+++ Library/Models/News.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Library.Models;
+
+public partial class News
+{
+    public int NewsId { get; set; }
+
+    public int UserId { get; set; }
+
+    public string? Title { get; set; }
+
+    public int ImageId { get; set; }
+}
