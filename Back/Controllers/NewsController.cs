c2e27bfb80f59cc120f83376c0aacccc691cc22c
REVISION-PC\Seth <sethetherald@duck.com>
2023-07-21 07:38:18 +0700

    Add initial news APIs.

---
+++ Back/Controllers/NewsController.cs
@@ -0,0 +1,83 @@
+﻿using AutoMapper;
+using Library.DAL;
+using Library.DTO;
+using Library.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Back.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]/[action]")]
+    public class NewsController : Controller
+    {
+        private readonly INewsRepository _news;
+
+        public NewsController(CoffeehouseSystemContext context, IMapper mapper)
+        {
+            _news = new NewsRepository(context, mapper);
+        }
+
+        [HttpGet]
+        public IActionResult List()
+        {
+            List<NewsInfo> news = _news.GetNews();
+            return Ok(news);
+        }
+
+        [Authorize(Roles = "User")]
+        [HttpPut]
+        public IActionResult Create(NewsInfo news)
+        {
+            try
+            {
+                _news.CreateNews(news);
+                _news.Save();
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [Authorize(Roles = "User")]
+        [HttpPatch]
+        public IActionResult Update(NewsInfo news)
+        {
+            try
+            {
+                _news.UpdateNews(news);
+                _news.Save();
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [Authorize(Roles = "User")]
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            try
+            {
+                _news.DeleteNews(id);
+                _news.Save();
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            _news.Dispose();
+            base.Dispose(disposing);
+        }
+    }
+}
