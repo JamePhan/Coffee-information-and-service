11ab2e54d59343ccd72b4f5b74d838ad4e79c7fc
JamePhan <jamephan129148@gmail.com>
2023-07-27 14:36:23 +0700

    Complete unit test Follow

---
+++ UnitTest/Follow/Follow_Test.cs
@@ -0,0 +1,108 @@
+using Library.DAL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using Library.DTO;
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using MimeKit;
+using MailKit.Net.Smtp;
+using Library.Models;
+using Back.Utilities;
+using Moq;
+using Back.Controllers;
+using Microsoft.EntityFrameworkCore;
+using AutoMapper;
+using Org.BouncyCastle.Asn1.Pkcs;
+using Castle.Core.Resource;
+using System.Reflection;
+
+namespace Capstone_UnitTest.Controller
+{
+    public class Follow_Test
+    {
+        private readonly Mock<CoffeehouseSystemContext> _mockContext;
+        private readonly Mock<IMapper> _mockMapper;
+
+        public Follow_Test()
+        {
+            _mockContext = new Mock<CoffeehouseSystemContext>();
+            _mockMapper = new Mock<IMapper>();
+        }
+
+        [Fact]
+        public void TC1_ViewListFollowingShopCoffee_Test()
+        {
+            FollowInfo follow = new FollowInfo();
+            follow.FollowingId = 1;
+            follow.UserId = 1;
+            follow.CustomerId = 1;
+            Test_Follow(follow);
+        }
+
+        [Fact]
+        public void TC2_ViewListFollowingShopCoffee_Test()
+        {
+            FollowInfo follow = new FollowInfo();
+            follow.FollowingId = 2;
+            follow.UserId = 2;
+            follow.CustomerId = 3;
+            Test_Follow(follow);
+        }
+
+        [Fact]
+        public void TC3_ViewListFollowingShopCoffee_Test()
+        {
+            FollowInfo follow = new FollowInfo();
+            follow.FollowingId = 3;
+            follow.UserId = 1;
+            follow.CustomerId = 2;
+            Test_Follow(follow);
+        }
+
+        [Fact]
+        public void TC4_ViewListFollowingShopCoffee_Test()
+        {
+            FollowInfo follow = new FollowInfo();
+            follow = null;
+            Test_Follow(follow);
+        }
+
+
+        public void Test_Follow(FollowInfo follow)
+        {
+            var followings = new List<Following>();
+
+            var mockDBFollowing = new Mock<DbSet<Following>>();
+            mockDBFollowing.As<IQueryable<Following>>().Setup(m => m.Provider).Returns(followings.AsQueryable().Provider);
+            mockDBFollowing.As<IQueryable<Following>>().Setup(m => m.Expression).Returns(followings.AsQueryable().Expression);
+            mockDBFollowing.As<IQueryable<Following>>().Setup(m => m.ElementType).Returns(followings.AsQueryable().ElementType);
+            mockDBFollowing.As<IQueryable<Following>>().Setup(m => m.GetEnumerator()).Returns(followings.AsQueryable().GetEnumerator());
+            _mockContext.SetupGet(m => m.Followings).Returns(mockDBFollowing.Object);
+
+            var followNew = new Following();
+            if (follow == null)
+            {
+                followNew = null;
+                _mockMapper.Setup(m => m.Map<FollowInfo, Following>(It.IsAny<FollowInfo>())).Returns(followNew);
+            }
+            else
+            {
+                followNew.FollowingId = 1;
+                followNew.UserId = 1;
+                followNew.CustomerId = 1;
+                _mockMapper.Setup(m => m.Map<FollowInfo, Following>(It.IsAny<FollowInfo>())).Returns(followNew);
+            }
+            FollowController followController = new FollowController(_mockContext.Object, _mockMapper.Object);
+
+            Assert.IsType<OkResult>(followController.Follow(follow));
+            _mockContext.Verify(c => c.Followings.Add(followNew), Times.Once);
+            _mockMapper.Verify(c => c.Map<FollowInfo, Following>(It.IsAny<FollowInfo>()), Times.Once);
+        }
+
+
+    }
+}
---
+++ UnitTest/Follow/ViewListFollowingShopCoffee_Test.cs
@@ -0,0 +1,134 @@
+using Library.DAL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using Library.DTO;
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using MimeKit;
+using MailKit.Net.Smtp;
+using Library.Models;
+using Back.Utilities;
+using Moq;
+using Back.Controllers;
+using Microsoft.EntityFrameworkCore;
+using AutoMapper;
+using Org.BouncyCastle.Asn1.Pkcs;
+using Castle.Core.Resource;
+
+namespace Capstone_UnitTest.Controller
+{
+    public class ViewListFollowingShopCoffee_Test
+    {
+        private readonly Mock<CoffeehouseSystemContext> _mockContext;
+        private readonly Mock<IMapper> _mockMapper;
+
+        public ViewListFollowingShopCoffee_Test()
+        {
+            _mockContext = new Mock<CoffeehouseSystemContext>();
+            _mockMapper = new Mock<IMapper>();
+        }
+
+        [Fact]
+        public void TC1_ViewListFollowingShopCoffee_Test()
+        {
+            Test_ViewListFollowingShopCoffee_Success(1);
+        }
+
+        [Fact]
+        public void TC2_ViewListFollowingShopCoffee_Test()
+        {
+            Test_ViewListFollowingShopCoffee_Success(2);
+        }
+
+        [Fact]
+        public void TC3_ViewListFollowingShopCoffee_Test()
+        {
+            Test_ViewListFollowingShopCoffee_Success(3);
+        }
+
+        [Fact]
+        public void TC4_ViewListFollowingShopCoffee_Test()
+        {
+            Test_ViewListFollowingShopCoffee_Success(0);
+        }
+
+        [Fact]
+        public void TC5_ViewListFollowingShopCoffee_Test()
+        {
+            Test_ViewListFollowingShopCoffee_Success(9);
+        }
+
+        [Fact]
+        public void TC6_ViewListFollowingShopCoffee_Test()
+        {
+            Test_ViewListCustomerFollowing_Fail(1);
+        }
+
+        public void Test_ViewListFollowingShopCoffee_Success(int id)
+        {
+            var followings = new List<Following>
+            {
+                new Following { FollowingId = 1, UserId = 1, CustomerId = 1 },
+                new Following { FollowingId = 2, UserId = 2, CustomerId = 2 },
+                new Following { FollowingId = 3, UserId = 1, CustomerId = 1 },
+                new Following { FollowingId = 4, UserId = 2, CustomerId = 1 },
+                new Following { FollowingId = 5, UserId = 1, CustomerId = 3 },
+                new Following { FollowingId = 5, UserId = 3, CustomerId = 3 },
+            };
+            var mockDBFollowing = new Mock<DbSet<Following>>();
+            mockDBFollowing.As<IQueryable<Following>>().Setup(m => m.Provider).Returns(followings.AsQueryable().Provider);
+            mockDBFollowing.As<IQueryable<Following>>().Setup(m => m.Expression).Returns(followings.AsQueryable().Expression);
+            mockDBFollowing.As<IQueryable<Following>>().Setup(m => m.ElementType).Returns(followings.AsQueryable().ElementType);
+            mockDBFollowing.As<IQueryable<Following>>().Setup(m => m.GetEnumerator()).Returns(followings.AsQueryable().GetEnumerator());
+            _mockContext.SetupGet(m => m.Followings).Returns(mockDBFollowing.Object);
+
+            var followInfos = new List<FollowInfo>
+            {
+                new FollowInfo { FollowingId = 1, UserId = 1, CustomerId = 1 },
+                new FollowInfo { FollowingId = 2, UserId = 2, CustomerId = 2 },
+                new FollowInfo { FollowingId = 3, UserId = 1, CustomerId = 1 },
+                new FollowInfo { FollowingId = 4, UserId = 2, CustomerId = 1 },
+                new FollowInfo { FollowingId = 5, UserId = 1, CustomerId = 3 },
+                new FollowInfo { FollowingId = 5, UserId = 3, CustomerId = 3 },
+            };
+            _mockMapper.Setup(m => m.Map<List<Following>, List<FollowInfo>>(It.IsAny<List<Following>>())).Returns(followInfos);
+
+            FollowController followController = new FollowController(_mockContext.Object, _mockMapper.Object);
+
+            Assert.IsType<OkObjectResult>(followController.CustomerList(id));
+            _mockContext.Verify(c => c.Followings, Times.Once);
+            _mockMapper.Verify(c => c.Map<List<Following>, List<FollowInfo>>(It.IsAny<List<Following>>()), Times.Once);
+        }
+
+        public void Test_ViewListCustomerFollowing_Fail(int id)
+        {
+            var followings = new List<Following>
+            {
+
+            };
+            var mockDBFollowing = new Mock<DbSet<Following>>();
+            mockDBFollowing.As<IQueryable<Following>>().Setup(m => m.Provider).Returns(followings.AsQueryable().Provider);
+            mockDBFollowing.As<IQueryable<Following>>().Setup(m => m.Expression).Returns(followings.AsQueryable().Expression);
+            mockDBFollowing.As<IQueryable<Following>>().Setup(m => m.ElementType).Returns(followings.AsQueryable().ElementType);
+            mockDBFollowing.As<IQueryable<Following>>().Setup(m => m.GetEnumerator()).Returns(followings.AsQueryable().GetEnumerator());
+            _mockContext.SetupGet(m => m.Followings).Returns(mockDBFollowing.Object);
+
+            var followInfos = new List<FollowInfo>
+            {
+
+            };
+            _mockMapper.Setup(m => m.Map<List<Following>, List<FollowInfo>>(It.IsAny<List<Following>>())).Returns(followInfos);
+
+            FollowController followController = new FollowController(_mockContext.Object, _mockMapper.Object);
+
+            Assert.IsType<NotFoundResult>(followController.CustomerList(id));
+            _mockContext.Verify(c => c.Followings, Times.Once);
+            _mockMapper.Verify(c => c.Map<List<Following>, List<FollowInfo>>(It.IsAny<List<Following>>()), Times.Once);
+        }
+
+    }
+}
