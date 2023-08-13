b1638aca585a547273a04d30be846347e8dcf565
REVISION-PC\Seth <sethetherald@duck.com>
2023-08-14 02:26:55 +0700

    Return image relative path.

--- Back/Controllers/ImageController.cs
+++ Back/Controllers/ImageController.cs
@@ -1,5 +1,6 @@
 ﻿using Back.Utilities;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.WebUtilities;
 using System.Web;
 
 namespace Back.Controllers
@@ -8,6 +9,13 @@ namespace Back.Controllers
     [Route("api/[controller]/[action]")]
     public class ImageController : Controller
     {
+        private readonly string _folderPath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/images/");
+
+        public ImageController()
+        {
+            Directory.CreateDirectory(_folderPath);
+        }
+
         [HttpPost]
         public IActionResult Upload(IFormFile file)
         {
@@ -18,13 +26,13 @@ namespace Back.Controllers
 
             string filename = file.FileName;
 
-            string uniqueName = Guid.NewGuid().ToString() + '_' + filename;
+            string uniqueName = WebEncoders.Base64UrlEncode(Guid.NewGuid().ToByteArray()) + '_' + filename;
 
-            var imagePath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/images/", uniqueName);
+            var imagePath = Path.Combine(_folderPath, uniqueName);
 
             file.CopyTo(new FileStream(imagePath, FileMode.Create));
 
-            return Ok(imagePath);
+            return Ok($"{Request.Scheme}://{Request.Host.Value}/images/{uniqueName}");
         }
     }
 }
