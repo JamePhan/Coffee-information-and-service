7f6d0ca1cc2d1d36bb53d52bdbfd078dc523bb6b
tholak309 <>
2023-09-26 08:52:37 +0700

    Update [id].tsx

--- Front-End/src/pages/shopList/[id].tsx
+++ Front-End/src/pages/shopList/[id].tsx
@@ -3,7 +3,6 @@ import Head from 'next/head';
 import { IInforUser } from 'src/shared/types/user.type';
 import { PreImage } from '@/components/common/PreImage';
 
-
 const containerStyle = {
     width: '100%',
     height: '400px', // Điều chỉnh kích thước bản đồ tùy ý
@@ -25,30 +24,40 @@ const UsersDetail = ({ shopListData }: Props) => {
             </Head>
             {shopListData && (
                 <div className='w-full h-full flex flex-col justify-start items-start dark:bg-[#1B1D35] bg-slate-100 pb-12'>
-                    <div className='relative w-full flex justify-between items-center mx-auto'>
-                        <div className='relative w-full flex-shrink-0 snap-start'>
-                            <PreImage
+                    <div className='w-full p-5 flex flex-col md:flex-row md:justify-center md:items-center gap-5'>
+                        <div className='w-full md:w-1/2 flex justify-center items-center'>
+                            <img
                                 src={shopListData.avatar}
-                                height={400}
-                                width={1980}
-                                layer={false}
-                                alt={'Banner News'}
-                                className='w-full object-cover'
+                                height={100}
+                                width={100}
+                                alt={'Avatar'}
+                                className='w-72 h-72 rounded-full object-cover mx-auto'
                             />
                         </div>
-                    </div>
-                    <div className='w-full p-5 flex flex-col justify-center items-start gap-3'>
-                        <div className='w-full flex justify-start items-center gap-5'>
-                            <h3 className='text-3xl'>{shopListData.coffeeShopName}</h3>
-                            <p className='text-slate-400'>Tác giả: {shopListData.coffeeShopName}</p>
-                        </div>
-                        <div className='mt-10 w-full h-full'>
-                            <p>{shopListData.email}</p>
-                            <div className='mt-5'>
-                                {/* Hiển thị Google Map */}
 
-                            </div>
+                        <div className='w-full md:w-1/2'>
+                            <h3 className='text-5xl mt-5'>{shopListData.coffeeShopName}</h3>
+                            <br></br><br></br>
+                            <h4 className='text-xl font-semibold'>Thông tin chi tiết:</h4>
+                            <p className='text-white-600'>Địa chỉ: {shopListData.address}</p>
+                            <p className='text-white-600'>Số điện thoại: {shopListData.phone}</p>
+                            <p className='text-white-600'>User ID: {shopListData.userId}</p>
+                            <p className='text-white-600'>Email:{shopListData.email}</p>
+
                         </div>
+                    </div>
+
+
+                    <div className='mt-10 w-full h-full'>
+                        {/* Hiển thị Google Map */}
+                        <iframe
+                            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3834.2199884156345!2d105.7938072155048!3d21.024424001245553!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3135ab06f6633685%3A0xfd1de3ba2e978c28!2zVGhlIENvZmZlZSBIb3VzZQ!5e0!3m2!1sen!2s!4v1630918443940!5m2!1sen!2s"
+                            width="100%"
+                            height="400"
+                            style={{ border: 0 }}
+                            // allowFullScreen=""
+                            loading="lazy"
+                        ></iframe>
                     </div>
                 </div>
             )}
